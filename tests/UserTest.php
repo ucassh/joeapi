2addf52c388ee14a0c2377827261e4130376644e
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-01-28 20:43:37 +0100

    added few new assertions (fan name, date of becoming a fan, reference from fan to user)

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests;
 
+use Joe\Fan;
 use Joe\Http\Client;
 use Joe\Http\Response;
 use Joe\User;
@@ -24,6 +25,17 @@ class UserTest extends TestsAbstract
         $user = new User('taksobietestuje');
         $user->setClient($client);
 
-        $this->assertEquals(1, $user->fans()->count(), 'Quantity of fans doesn\'t match');
+        $fans = $user->fans();
+        $this->assertEquals(1, $fans->count(), 'Quantity of fans doesn\'t match');
+
+        /** @var Fan $fan */
+        foreach ($fans as $fan) {
+            $this->assertSame($user, $fan->whom(), "Fan instance should be a fan of User instance");
+        }
+
+        $this->assertEquals("onlyonefan", $fan->id(), "Fan ID mismatch");
+
+        $fanSince = $fan->since()->format("Y-m-d H:i:s");
+        $this->assertEquals("2015-06-11 20:58:15", $fanSince, 'Incorrect \'became a fan\' time ');
     }
 }
