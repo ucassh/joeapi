561d4bd1e88143f8dcc32b87f08e6b3e45d3ed66
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-01-29 12:25:36 +0100

    extraction of mocking code to separate methods in parent class

--- tests/TestsAbstract.php
+++ tests/TestsAbstract.php
@@ -2,12 +2,45 @@
 
 namespace Tests;
 
+use Joe\Http\Client;
+use Joe\Http\Response;
+
 abstract class TestsAbstract extends \PHPUnit_Framework_TestCase
 {
     public function __construct()
     {
+        parent::__construct();
         if (!defined('MAX_FILE_SIZE')) {
             define('MAX_FILE_SIZE', 600000000);
         }
+    }
+
+
+    /**
+     * @param string $body
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function mockResponse($body = '')
+    {
+        $response = $this->createMock(Response::class);
+        $response
+            ->expects($this->any())
+            ->method('getBody')
+            ->willReturn($body);
+        return $response;
+    }
+
+    /**
+     * @param $response
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function mockClient($response)
+    {
+        $client = $this->createMock(Client::class);
+        $client
+            ->expects($this->any())
+            ->method('request')
+            ->willReturn($response);
+        return $client;
     }
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,25 +2,14 @@
 namespace Tests;
 
 use Joe\Fan;
-use Joe\Http\Client;
-use Joe\Http\Response;
 use Joe\User;
 
 class UserTest extends TestsAbstract
 {
     public function testGetUserFans()
     {
-        $response = $this->createMock(Response::class);
-        $response
-            ->expects($this->any())
-            ->method('getBody')
-            ->willReturn(file_get_contents('tests/files/fans.html'));
-
-        $client = $this->createMock(Client::class);
-        $client
-            ->expects($this->any())
-            ->method('request')
-            ->willReturn($response);
+        $response = $this->mockResponse(file_get_contents('tests/files/fans.html'));
+        $client = $this->mockClient($response);
 
         $user = new User('taksobietestuje');
         $user->setClient($client);
