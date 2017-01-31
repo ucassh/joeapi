7b4a677dd3d95137f0cced4b4526154a719500c6
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-01-31 14:06:56 +0100

    basic httpClient keeper (to be replaced by some DI container e.g. like Pimple)

---
+++ src/Connection.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Joe;
+
+use Joe\Http\GuzzleClient;
+use Joe\Http\GuzzleReques;
+use Joe\Http\Client;
+
+class Connection
+{
+    const ADDRESS = 'http://joemonster.org';
+
+    /** @var  Client */
+    private static $httpClient;
+
+
+    /**
+     * @param $login
+     * @param $password
+     * @return Client
+     */
+    public static function login($login, $password)
+    {
+        Connection::$httpClient = new GuzzleClient();
+
+        Connection::$httpClient->request('GET', Connection::ADDRESS . '/logowanie', ['headers' => []]);
+
+        Connection::$httpClient->request('POST', Connection::ADDRESS . '/login_check', [
+            'form_params' => [
+                '_username' => $login,
+                '_password' => $password,
+                'op' => 'login'
+            ],
+        ]);
+
+        return Connection::$httpClient;
+    }
+
+    public static function client(array $config = [])
+    {
+        return isset(Connection::$httpClient)
+            ? Connection::$httpClient
+            : new GuzzleClient($config);
+    }
+
+}
