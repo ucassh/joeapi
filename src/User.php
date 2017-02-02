013ed98e01665228834582c387cbd12028553cda
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-02-02 21:36:30 +0100

    first iteration of method about; it definitely need to be moved to separate class

--- src/User.php
+++ src/User.php
@@ -3,6 +3,7 @@
 namespace Joe;
 
 use Joe\Http\Client;
+use simplehtmldom_1_5\simple_html_dom_node;
 use Sunra\PhpSimple\HtmlDomParser;
 
 class User
@@ -86,9 +87,49 @@ class User
         return $collection;
     }
 
+    //TODO refactor method to class
     public function about()
     {
+        $res = $this->client->request('GET', Connection::ADDRESS . '/bojownik/' . $this->id);
+        $body = $res->getBody();
+        $html = HtmlDomParser::str_get_html($body);
 
+        $profilLeft = $html->find('div.profilLeft');
+
+        if (!isset($profilLeft[0])) {
+            throw new \Exception('Object containing informations was not found.');
+        }
+
+        $about = $profilLeft[0];
+        $innerText = $profilLeft[0]->innertext();
+
+        $content = [];
+        $content['hello'] = strip_tags(substr($innerText, 0, strpos($innerText, '<dl>')));
+
+        $dls = $about->find('dl');
+        if (isset($dls[0])) {
+            $content['Main'] = $this->getPropertiesFromDl($dls[0]);
+        }
+
+        $headers = $about->find('h3');
+        foreach ($headers as $h) {
+            $block = $h->text();
+            /** @var $dl simple_html_dom_node */
+            $dl = $h->next_sibling();
+
+            if ($dl->tag == 'dl') {
+                $content[$block] = $this->getPropertiesFromDl($dl);
+            } else {
+                Log::info('Exception was found', [$dl->outertext()]);
+            }
+        }
+
+        $scripts = $about->find('script');
+        if (isset($scripts[1])) {
+            $content['place'] = str_replace(';', ";\n", $scripts[1]->innertext());
+        }
+
+        return $content;
     }
 
     public function sended()
@@ -143,5 +184,23 @@ class User
     public function id()
     {
         return $this->id;
+    }
+
+    /**
+     * @param $dl
+     * @return array
+     */
+    protected function getPropertiesFromDl($dl)
+    {
+        $content = [];
+        $props = $dl->children();
+        foreach ($props as $prop) {
+            if ($prop->tag == 'dt') {
+                $key = $prop->text();
+            } elseif ($prop->tag == 'dd') {
+                $content[$key] = $prop->text();
+            }
+        }
+        return $content;
     }
 }
