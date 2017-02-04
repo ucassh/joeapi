17c4cc551169303c3783d7208c5a9c424ffe09b1
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-02-05 00:37:22 +0100

    trait for basic functionality (which is htttp client managemenet)

---
+++ src/User/ClientTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Joe\User;
+
+use Joe\Http\Client;
+
+trait ClientTrait
+{
+
+    /** @var Client $client */
+    private $client;
+
+    /**
+     * @return mixed
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param mixed $client
+     */
+    public function setClient($client)
+    {
+        $this->client = $client;
+    }
+}
