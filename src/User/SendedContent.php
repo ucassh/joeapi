10e61a1b4400c154fda1fc21936433233a956552
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-02-12 12:57:47 +0100

    typo corrected in variable + added casting to int

--- src/User/SendedContent.php
+++ src/User/SendedContent.php
@@ -54,19 +54,19 @@ class SendedContent
         $img = $node->find('img.itemBigThumb');
         $addTime = $node->find('div.small span');
         $counts = $node->find('div.small b');
-        $counts = isset($counz[0]) ? array_map('trim', explode('&middot,', $counts[0]->text())) : ['', '', ''];
+        $counts = isset($counts[0]) ? array_map('trim', explode('&middot;', $counts[0]->text())) : ['', '', ''];
         return [
             'author' => $this->user,
             'title' => isset($link[0]) ? trim($link[0]->text()) : '',
             'link' => isset($link[0]) ? $link[0]->href : '',
-            'id' => substr($beginOfIf, 0, strpos($beginOfIf, '/')),
+            'id' => (int)substr($beginOfIf, 0, strpos($beginOfIf, '/')),
             'snippet_msg' => isset($description[0]) ? trim($description[0]->text()) : '',
             'thumbnail' => isset($img[0]) ? $img[0]->src : '',
             'time' => isset($addTime[1]) ? trim($addTime[1]->text()) : '',
             'date' => new \DateTime(isset($addTime[1]) ? $addTime[1]->getAttribute('title') : null),
-            'ok_count' => $counts[0],
-            'views_count' => $counts[1],
-            'comments_count' => $counts[2]
+            'ok_count' => (int)$counts[0],
+            'views_count' => (int)str_replace([' ','x'], '', $counts[1]),
+            'comments_count' => (int)$counts[2]
         ];
     }
 }
