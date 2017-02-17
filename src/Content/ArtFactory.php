db61fb8961393b7b12410c4593eda5ca77d33515
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-02-17 15:05:04 +0100

    factories development - added field names in ArtFactory and condition in all setters

--- src/Content/ArtFactory.php
+++ src/Content/ArtFactory.php
@@ -11,13 +11,13 @@ class ArtFactory extends ContentFactory
 
     public function create(array $params = [])
     {
-        return $this->createFromListing($params[''])
-            ->getTags($params[''])
-            ->getViewsOnPage($params[''])
-            ->getFullDescription($params[''])
-            ->getAddingTimeOnPage($params[''])
-            ->getOkCountOnPage($params[''])
-            ->getNotOkCount($params['']);
+        return $this->createFromListing($params)
+            ->setTags(isset($params['tags']) ? $params['tags'] : null)
+            ->setViewsOnPage(isset($params['main_views_count']) ? $params['main_views_count'] : null)
+            ->setFullDescription(isset($params['full_description']) ? $params['full_description'] : null)
+            ->setAddingTimeOnPage(isset($params['main_adding_time']) ? $params['main_adding_time'] : null)
+            ->setOkCountOnPage(isset($params['main_ok_count']) ? $params['main_ok_count'] : null)
+            ->setNotOkCount(isset($params['main_not_ok_count']) ? $params['main_not_ok_count'] : null);
     }
 
 }
--- src/Content/ContentFactory.php
+++ src/Content/ContentFactory.php
@@ -12,16 +12,17 @@ abstract class ContentFactory
     public function createFromListing(array $params = [])
     {
         $cls = 'Joe\Content\\'.$this->clazz;
-        $art = new $cls($params['id']);
-        return $art->setAddingTimeFromSnippet($params['time'])
-            ->setAddingDateFromSnippet($params['date'])
-            ->setAuthor($params['author'])
-            ->setLink($params['link'])
-            ->setOkCountFromSnippet($params['ok_count'])
-            ->setCommentsCountFromSnippet($params['comments_count'])
-            ->setSnippetMsg($params['snippet_msg'])
-            ->setThumbnail($params['thumbnail'])
-            ->setTitle($params['title'])
-            ->setViewsFromSnippet($params['views_count']);
+        /** @var Content $content */
+        $content = new $cls(isset($params['id']) ? $params['id'] : null);
+        return $content->setAddingTimeFromSnippet(isset($params['time']) ? $params['time'] : null)
+            ->setAddingDateFromSnippet(isset($params['date']) ? $params['date'] : null)
+            ->setAuthor(isset($params['author']) ? $params['author'] : null)
+            ->setLink(isset($params['link']) ? $params['link'] : null)
+            ->setOkCountFromSnippet(isset($params['ok_count']) ? $params['ok_count'] : null)
+            ->setCommentsCountFromSnippet(isset($params['comments_count']) ? $params['comments_count'] : null)
+            ->setSnippetMsg(isset($params['snippet_msg']) ? $params['snippet_msg'] : null)
+            ->setThumbnail(isset($params['thumbnail']) ? $params['thumbnail'] : null)
+            ->setTitle(isset($params['title']) ? $params['title'] : null)
+            ->setViewsFromSnippet(isset($params['views_count']) ? $params['views_count'] : null);
     }
 }
