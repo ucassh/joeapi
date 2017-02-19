c6ea743b0b9ae4f3277fdd4ebec828500be22ab7
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-02-19 21:43:03 +0100

    enlargement of ContentScrapers interface (key fields have now dedicted methods to scrap)

--- src/Content/Scraper/ContentScraper.php
+++ src/Content/Scraper/ContentScraper.php
@@ -10,10 +10,25 @@ abstract class ContentScraper
     use ClientTrait;
 
     const ADDRESS = 'http://joemonster.org';
+    protected $id;
 
-    public function __construct(Client $client) {
+    public function __construct($id, Client $client) {
+        $this->id = $id;
         $this->client = $client;
     }
 
-    public abstract function getSite($id);
+    public abstract function getSite();
+    public abstract function getAuthor();
+    public abstract function getLink();
+    public abstract function getId();
+    public abstract function getTitle();
+    public abstract function getViewsCount();
+    public abstract function getAddingTime();
+    public abstract function getOkCount();
+    public abstract function getCommentsCount();
+    public abstract function getContent();
+    public abstract function getNotOkCount();
+    public abstract function getTags();
+    public abstract function getComments();
+    public abstract function getLikers();
 }
