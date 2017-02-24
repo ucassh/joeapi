7e9fde0357225b8475758aabcf4f0df7e9557bae
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-02-24 23:51:38 +0100

    init commit of skeleton of methods in FilmScraper

--- src/Content/Scraper/FilmScraper.php
+++ src/Content/Scraper/FilmScraper.php
@@ -2,10 +2,82 @@
 
 namespace Joe\Content\Scraper;
 
+use simplehtmldom_1_5\simple_html_dom;
+
 class FilmScraper extends ContentScraper
 {
     public function getSite($id)
     {
         
+    }
+
+    protected function contentHtml(simple_html_dom $fullHtml = null)
+    {
+        // TODO: Implement contentHtml() method.
+    }
+
+    public function getAuthor()
+    {
+        // TODO: Implement getAuthor() method.
+    }
+
+    public function getAddress()
+    {
+        // TODO: Implement getAddress() method.
+    }
+
+    public function getTitle()
+    {
+        // TODO: Implement getTitle() method.
+    }
+
+    public function getViewsCount()
+    {
+        // TODO: Implement getViewsCount() method.
+    }
+
+    public function getAddingTime()
+    {
+        // TODO: Implement getAddingTime() method.
+    }
+
+    public function getOkCount()
+    {
+        // TODO: Implement getOkCount() method.
+    }
+
+    public function getCommentsCount()
+    {
+        // TODO: Implement getCommentsCount() method.
+    }
+
+    public function getContent()
+    {
+        // TODO: Implement getContent() method.
+    }
+
+    public function getNotOkCount()
+    {
+        // TODO: Implement getNotOkCount() method.
+    }
+
+    public function getTags()
+    {
+        // TODO: Implement getTags() method.
+    }
+
+    public function getComments()
+    {
+        // TODO: Implement getComments() method.
+    }
+
+    public function getLikers()
+    {
+        // TODO: Implement getLikers() method.
+    }
+
+    public function getAgeRestrictions()
+    {
+        // TODO: Implement getAgeRestrictions() method.
     }
 }
