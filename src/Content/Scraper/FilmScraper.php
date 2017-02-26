026eb9dc575591baab386e686b136afbca58dca4
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-02-26 20:52:39 +0100

    added implementation to half of FilmScraper class

--- src/Content/Scraper/FilmScraper.php
+++ src/Content/Scraper/FilmScraper.php
@@ -2,33 +2,40 @@
 
 namespace Joe\Content\Scraper;
 
+use Joe\Content\FilmFactory;
+use Joe\Helper\CommentsHelper;
+use Joe\User;
 use simplehtmldom_1_5\simple_html_dom;
+use simplehtmldom_1_5\simple_html_dom_node;
 
 class FilmScraper extends ContentScraper
 {
-    public function getSite($id)
+    public function getSite()
     {
-        
+        $artFactory = new FilmFactory;
+        return $artFactory->createFromScraper($this);
     }
 
     protected function contentHtml(simple_html_dom $fullHtml = null)
     {
-        // TODO: Implement contentHtml() method.
+        $film = $fullHtml->find('#main');
+        return isset($film[0]) ? $film[0] : null;
     }
 
     public function getAuthor()
     {
-        // TODO: Implement getAuthor() method.
+        $username = $this->getElemTxtIfSet('.mtv-video-details a');
+        return new User($username);
     }
 
     public function getAddress()
     {
-        // TODO: Implement getAddress() method.
+        return $this::ADDRESS . '/filmy/' . $this->id;
     }
 
     public function getTitle()
     {
-        // TODO: Implement getTitle() method.
+        return $this->getElemTxtIfSet('h1.playerTitle b.title');
     }
 
     public function getViewsCount()
@@ -63,21 +70,38 @@ class FilmScraper extends ContentScraper
 
     public function getTags()
     {
-        // TODO: Implement getTags() method.
+        return array_map(function (simple_html_dom_node $val) {
+            return $val->text();
+        }, $this->html->find('span.tags a'));
     }
 
     public function getComments()
     {
-        // TODO: Implement getComments() method.
+        $scripts = implode(PHP_EOL, array_map(function (simple_html_dom_node $val) {
+            return str_replace("\t", PHP_EOL, $val->innertext());
+        }, $this->html->find('script')));
+
+        return CommentsHelper::extractComments($scripts);
     }
 
     public function getLikers()
     {
-        // TODO: Implement getLikers() method.
+        $collection = new \ArrayObject;
+        $html = $this->getPage(self::ADDRESS . '/who.php?sid=' . $this->getId() . '&op=favourited-movies');
+        foreach ($html->find('div.userBox') as $div) {
+            $collection->append(new User(trim($div->text())));
+        }
+        return $collection;
     }
 
     public function getAgeRestrictions()
     {
         // TODO: Implement getAgeRestrictions() method.
+    }
+
+    public function getDescription()
+    {
+        $title = $this->html->find('mtvDescription');
+        return isset($title[0]) ? trim($title[0]->text()) : '';
     }
 }
