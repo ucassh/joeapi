ca992be33dd51145cf7637fb17a17be1a7770295
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-02 21:51:39 +0100

    added throwing Exception

--- src/User/ClientTrait.php
+++ src/User/ClientTrait.php
@@ -29,14 +29,21 @@ trait ClientTrait
     }
 
     /**
-     * @param $address
+     * @param string $address
      * @return \simplehtmldom_1_5\simple_html_dom
+     * @throws \Exception
      */
     public function getPage($address)
     {
         /** @var Response $res */
         $res = $this->client->request('GET', $address);
         $body = $res->getBody();
-        return HtmlDomParser::str_get_html($body);
+        $dom = HtmlDomParser::str_get_html($body);
+
+        if ($dom !== false) {
+            throw new \Exception('No document found');
+        }
+
+        return $dom;
     }
 }
