de18f5e34ddefdf0101368e29583f8e39eca26f9
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-03 21:07:19 +0100

    removed unnecessary code

--- src/Content/ArtFactory.php
+++ src/Content/ArtFactory.php
@@ -8,15 +8,4 @@ class ArtFactory extends ContentFactory
     {
         $this->clazz = 'Art';
     }
-
-    public function create(array $params = [])
-    {
-        return parent::create($params)
-            ->setTags(isset($params['tags']) ? $params['tags'] : null)
-            ->setViewsCount(isset($params['main_views_count']) ? $params['main_views_count'] : null)
-            ->setContent(isset($params['full_description']) ? $params['full_description'] : null)
-            ->setOkCount(isset($params['main_ok_count']) ? $params['main_ok_count'] : null)
-            ->setNotOkCount(isset($params['main_not_ok_count']) ? $params['main_not_ok_count'] : null);
-    }
-
 }
