2286e87e05edfdfb7802fa3cec3c022508557c34
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-05 21:14:36 +0100

    added missing methods

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -54,7 +54,7 @@ class UserTest extends TestsAbstract
         $this->assertSame(1478468630, $comment[0]->getUnixTimestamp(), "UnieTimestamp read incorrect");
         $this->assertSame("vid", $comment[0]->getType(), "Type read incorrect");
         $this->assertSame(1, $comment[0]->getToplevel(), "TopLevel read incorrect");
-
+        $this->assertSame('/filmy/5432#cm81823304797', $comment[0]->getLink(), "Link is incorrect");
 
         $this->assertSame("Comment No. 2", $comment[1]->getMessage(), "Message read incorrect");
         $this->assertSame("2016-11-06 14:23:47", $comment[1]->getDate()->format("Y-m-d H:i:s"), "Date read incorrect");
@@ -67,6 +67,7 @@ class UserTest extends TestsAbstract
         $this->assertSame(1478438627, $comment[1]->getUnixTimestamp(), "UnieTimestamp read incorrect");
         $this->assertSame("art", $comment[1]->getType(), "Type read incorrect");
         $this->assertSame(1, $comment[1]->getToplevel(), "TopLevel read incorrect");
+        $this->assertSame('/art/4444#cm2222', $comment[1]->getLink(), "Link is incorrect");
     }
 
     /**
