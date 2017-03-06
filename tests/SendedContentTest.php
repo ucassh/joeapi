17427f8ef1b190461b1113a3f5be7ffb6922a4bd
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-06 21:19:41 +0100

    added unit test

--- tests/SendedContentTest.php
+++ tests/SendedContentTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Joe\Content\ArtSnippet;
+use Joe\Content\FilmSnippet;
 use Joe\User\SendedContent;
 
 class SendedContentTest extends TestsAbstract
@@ -28,7 +29,7 @@ class SendedContentTest extends TestsAbstract
         $this->assertEquals(302, $sended->filmPagesQuantity());
     }
 
-    public function testGetArticlesPageCount()
+    public function testGetArticlesPage()
     {
         $response = $this->mockResponse(file_get_contents('tests/files/articles_list.html'));
         $client = $this->mockClient($response);
@@ -49,6 +50,29 @@ class SendedContentTest extends TestsAbstract
         $this->assertEquals('8 kobiet, które są wdzięczne za każdy kilogram swojego ciała', $artArr[0]->getTitle());
         $this->assertEquals(102755, $artArr[0]->getViewsCount());
         $this->assertEquals('/art/38744/8_kobiet_ktore_sa_wdzieczne_za_kazdy_kilogram_swojego_ciala', $artArr[0]->getLink());
+    }
+
+    public function testGetFilmsPage()
+    {
+        $response = $this->mockResponse(file_get_contents('tests/files/films_list.html'));
+        $client = $this->mockClient($response);
+        $user = $this->mockUser($client);
+
+        $sended = new SendedContent($user);
+        $articles = $sended->getFilmsPage();
+        $this->assertEquals(10, $articles->count());
+        /** @var FilmSnippet[] $filmArr */
+        $filmArr = $articles->getArrayCopy();
+        //$this->assertEquals('profil', $filmArr[0]->getAuthor()->nickName()); // not gonna test this, cause nickname cames from mockobject, and it's not same as in mockfile
+        $this->assertEquals(new \DateTime('2017-03-04'), $filmArr[0]->getAddingTime());
+        $this->assertEquals(36, $filmArr[0]->getCommentsCount());
+        $this->assertEquals(82233, $filmArr[0]->getId());
+        $this->assertEquals(65, $filmArr[0]->getOkCount());
+        $this->assertEquals('Podczas testów w Katalonii widzowie mogli poczuć już zbliżający się przyszły sezon. Kolejne zmiany - czy na lepsze? Sami oceńcie.', $filmArr[0]->getContent());
+        $this->assertEquals('http://download.joemonster.org/th/p346934.jpg', $filmArr[0]->getThumbnail());
+        $this->assertEquals('F1 w 2017 roku - wyjazd bolidów z pitstopu, nowy wygląd, start i dźwięk', $filmArr[0]->getTitle());
+        $this->assertEquals(17400, $filmArr[0]->getViewsCount());
+        $this->assertEquals('/filmy/82233/F1_w_2017_roku_wyjazd_bolidow_z_pitstopu_nowy_wyglad_start_i_dzwiek_', $filmArr[0]->getLink());
     }
 
     public function testArtPagesQuantityButBoPagesInListing()
