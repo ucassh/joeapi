d81fb53bf0db325cca471987483866bbfa6b12c5
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-17 06:08:04 +0100

    aded unit test for About lass

--- tests/AboutTest.php
+++ tests/AboutTest.php
@@ -41,4 +41,44 @@ class AboutTest extends TestsAbstract
         $this::assertArrayHasKey('Status konta', $basicInfo);
         $this::assertArrayHasKey('GG', $basicInfo);
     }
+
+    public function testCategorizedInfo()
+    {
+        $info = $this->about->getCategorisedInfo();
+        $this::assertArrayHasKey('WAŻNE  INFORMACJE O MNIE', $info);
+        $important = $info['WAŻNE  INFORMACJE O MNIE'];
+        $this::assertArrayHasKey('Mogę załatwić', $important);
+        $this::assertArrayHasKey('Jak długiego mam?', $important);
+        $this::assertArrayHasKey('W przyszłości chcę zostać', $important);
+        $this::assertArrayHasKey('Ważne osoby, które znam', $important);
+        $this::assertArrayHasKey('Numer mojej karty kredytowej', $important);
+        $this::assertArrayHasKey('Pin do mojej karty', $important);
+        $this::assertArrayHasKey('Jak wiele jestem w stanie', $important);
+        $this::assertArrayHasKey('Jak to lubię', $important);
+        $this::assertArrayHasKey('Kiedy', $important);
+        $this::assertArrayHasKey('Która godzina', $important);
+        $this::assertArrayHasKey('Miseczka', $important);
+        $this::assertArrayHasKey('Na pączka', $important);
+        $this::assertArrayHasKey('Strony, które odwiedzam', $important);
+
+        $this::assertArrayHasKey('CO LUBIĘ', $info);
+        $like = $info['CO LUBIĘ'];
+        $this::assertArrayHasKey('Film', $like);
+        $this::assertArrayHasKey('Książka', $like);
+        $this::assertArrayHasKey('Piosenka', $like);
+        $this::assertArrayHasKey('Komedia', $like);
+        $this::assertArrayHasKey('Dramat', $like);
+        $this::assertArrayHasKey('Kabaret', $like);
+        $this::assertArrayHasKey('Gra', $like);
+        $this::assertArrayHasKey('Pozycja', $like);
+        $this::assertArrayHasKey('Jedzenie', $like);
+        $this::assertArrayHasKey('Seks', $like);
+        $this::assertArrayHasKey('Władza', $like);
+        $this::assertArrayHasKey('Alkohol', $like);
+
+        $this::assertArrayHasKey('Aktywność bojownicza', $info);
+        $acitivity = $info['Aktywność bojownicza'];
+        $this::assertArrayHasKey('Profil oglądany', $acitivity);
+
+    }
 }
