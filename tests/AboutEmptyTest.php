ffdf859818858c97b8231347c872e57988e1f46f
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-18 21:12:34 +0100

    About::getBasicInfo empty response uit test

---
+++ tests/AboutEmptyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests;
+
+use Joe\User\About;
+
+class AboutEmptyTest extends TestsAbstract
+{
+    /** @var About */
+    private $about;
+
+    protected function setUp()
+    {
+        $response = $this->mockResponse(file_get_contents('tests/files/taksobietestuje.less.about.html'));
+        $client = $this->mockClient($response);
+        $user = $this->mockUser($client);
+
+        $this->about = new About($user);
+    }
+
+    protected function tearDown()
+    {
+        $this->about = null;
+    }
+
+    public function testEmptyBasicInfo()
+    {
+        $basicInfo = $this->about->getBasicInfo();
+        $this::assertEmpty($basicInfo);
+    }
+}
