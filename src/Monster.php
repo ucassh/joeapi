be84da12c15dc39cc5993165946e33c6ac18b797
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-23 00:32:37 +0100

    removed constant initialize

--- src/Monster.php
+++ src/Monster.php
@@ -2,10 +2,6 @@
 
 namespace Joe;
 
-if (!defined('MAX_FILE_SIZE')) {
-    define('MAX_FILE_SIZE', 600000000);
-}
-
 class Monster
 {
     public function __construct($login = null, $password = null)
