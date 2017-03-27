d2b5cc1dc25946a2c279770e3adff86ab26ba628
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-27 18:48:43 +0200

    unit test

--- tests/Content/FilmTest.php
+++ tests/Content/FilmTest.php
@@ -6,5 +6,19 @@ use Joe\Content\Film;
 
 class FilmTest extends TestsAbstract
 {
+    public function testCreateFilm()
+    {
+        $film = new Film(1);
+        $this::assertSame(Film::class, get_class($film));
+        return $film;
+    }
 
+    /**
+     * @depends testCreateFilm
+     */
+    public function testFilmGetAndSet(Film $film)
+    {
+        $film->setCategory($value = '123');
+        self::assertSame($value, $film->getCategory());
+    }
 }
