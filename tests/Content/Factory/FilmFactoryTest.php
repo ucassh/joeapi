7c4f9a1c3d8f5c7480e5f4b3aacc035d625e4970
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-28 19:19:48 +0200

    film factory unit test

--- tests/Content/Factory/FilmFactoryTest.php
+++ tests/Content/Factory/FilmFactoryTest.php
@@ -1,8 +1,24 @@
 <?php
 
+namespace Tests;
+
 use Joe\Content\Factory\FilmFactory;
 
-class FilmFactoryTest extends \Tests\TestsAbstract
-{
+class ChildFilmFactory extends FilmFactory{
+    /**
+     * @return mixed
+     */
+    public function getClazz()
+    {
+        return $this->clazz;
+    }
+}
 
+class FilmFactoryTest extends TestsAbstract
+{
+    public function testCreateArtFactory()
+    {
+        $child = new ChildFilmFactory();
+        $this::assertSame('Film', $child->getClazz());
+    }
 }
