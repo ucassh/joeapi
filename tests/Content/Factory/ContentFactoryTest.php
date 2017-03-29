1ff70cd1b383a683796c939bc0ecdda2524c200b
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-03-29 21:57:19 +0200

    unit test data

--- tests/Content/Factory/ContentFactoryTest.php
+++ tests/Content/Factory/ContentFactoryTest.php
@@ -1,8 +1,36 @@
 <?php
+namespace Tests;
 
+use Joe\Content\Content;
 use Joe\Content\Factory\ContentFactory;
+use Joe\User;
 
-class ContentFactoryTest extends \Tests\TestsAbstract
+class ContentFactoryTest extends TestsAbstract
 {
+    protected $data;
+
+    protected function setUp()
+    {
+        $user = $this->getMockBuilder(User::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->data = [];
+        $this->data['id'] = 1;
+        $this->data['time'] = new \DateTime('2000-01-01 00:00:00');
+        $this->data['author'] = $user;
+        $this->data['link'] = 'http://joemonster.org';
+        $this->data['comments_count'] = 123;
+        $this->data['content'] = 'the quick brown fox jumps over the lazy dog';
+        $this->data['title'] = 'Fox & Dog';
+        $this->data['ok_count'] = 321;
+        $this->data['not_ok_count'] = 1;
+        $this->data['views_count'] = 999;
+        $this->data['age_restrictions'] = false;
+        $this->data['comments'] = new \ArrayObject;
+        $this->data['likers'] = new \ArrayObject;
+        $this->data['tags'] = ['tag1', 'tag2', 'tag3', 'tag4'];
+        $this->data['description'] = 'tldr';
+    }
 
 }
