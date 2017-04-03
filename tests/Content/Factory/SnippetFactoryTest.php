d567d1621e943ad7ebf43ceeaeef8388538d6267
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-03 22:01:33 +0200

    unit test

--- tests/Content/Factory/SnippetFactoryTest.php
+++ tests/Content/Factory/SnippetFactoryTest.php
@@ -1,8 +1,42 @@
 <?php
 
+namespace Tests;
+
+use Joe\Content\ArtSnippet;
+use Joe\Content\Factory\ContentFactory;
 use Joe\Content\Factory\SnippetFactory;
 
-class SnippetFactoryTest extends \Tests\TestsAbstract
+class SnippetFactoryTest extends AbstractFactoryTest
 {
 
+    protected function prepareData()
+    {
+        parent::prepareData();
+        $this->data['thumbnail'] = 'some small thumbnail';
+    }
+
+    protected function prepareFactory()
+    {
+        return new SnippetFactory($this->getClassOfContent());
+    }
+
+    protected function createContent(ContentFactory $factory)
+    {
+        return $factory->create($this->data);
+    }
+
+    public function testContentValues()
+    {
+        $this->prepare();
+
+        /** @var ArtSnippet $content */
+        $content = $this->content;
+        $this::assertSame($this->data['thumbnail'], $content->getThumbnail());
+
+    }
+
+    protected function getClassOfContent()
+    {
+        return 'Joe\Content\ArtSnippet';
+    }
 }
