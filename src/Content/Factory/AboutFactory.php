c53d2dc8f1b264abc5537f2e148e7bf7c572a81c
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-12 23:06:14 +0200

    added factory

--- src/Content/Factory/AboutFactory.php
+++ src/Content/Factory/AboutFactory.php
@@ -2,19 +2,41 @@
 
 namespace Joe\Content\Factory;
 
-use Joe\Content\Scraper\AbstractScraper;
+use Joe\Content\Scraper\AboutScraper;
+use Joe\User\About;
 
 class AboutFactory
 {
 
+    /**
+     * @param array $params
+     * @return About
+     */
     public function create(array $params = [])
     {
-
+        return (new About)
+            ->setBasicInfo($params['basic'] ?: [])
+            ->setHelloMessage($params['hello'] ?: '')
+            ->setId($params['user_id'] ?: null)
+            ->setLocalization($params['localization'] ?: [])
+            ->setProperties($params['properties'] ?: [])
+            ->setUser($params['user'] ?: null);
     }
 
-    public function createFromScraper(AbstractScraper $scraper)
+    /**
+     * @param AboutScraper $scraper
+     * @return About
+     */
+    public function createFromScraper(AboutScraper $scraper)
     {
-        return $this->create([]);
+        return $this->create([
+            'user_id' => $scraper->getUserId(),
+            'basic' => $scraper->getBasicInfo(),
+            'properties' => $scraper->getCategorisedInfo(),
+            'hello' => $scraper->getHelloMessage(),
+            'localization' => $scraper->getLocalization(),
+            'user' => $scraper->getUser(),
+        ]);
     }
 
 }
