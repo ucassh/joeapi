ef4c07152e7524d1bb9d852f165ae6c1805badbe
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-13 23:19:32 +0200

    get blog subscriptions

--- src/Content/Scraper/SubscriptionsScraper.php
+++ src/Content/Scraper/SubscriptionsScraper.php
@@ -7,6 +7,7 @@ use Joe\Http\Client;
 use Joe\User;
 use Joe\User\ClientTrait;
 use simplehtmldom_1_5\simple_html_dom;
+use simplehtmldom_1_5\simple_html_dom_node;
 
 class SubscriptionsScraper extends AbstractScraper
 {
@@ -30,8 +31,24 @@ class SubscriptionsScraper extends AbstractScraper
     protected function contentHtml(simple_html_dom $fullHtml = null)
     {
         $node = $fullHtml->find('#userProfileContent');
-        return $node[0] ?: $fullHtml;
+        return $node[0] ?: null;
     }
 
+    public function blogSubscriptions()
+    {
+        $collecion = new \ArrayObject;
+        /** @var simple_html_dom_node $node */
+        $node = $this->content->find('h2', 0);
+        if ($node && $node->text() == 'Subskrybenci bloga') {
+            /** @var simple_html_dom_node $sibling */
+            $sibling = $node->nextSibling();
+            foreach ($sibling->childNodes() as $child) {
+                if ($child->class == 'avatar') {
+                    $collecion->append(new User(trim($child->text())));
+                }
+            }
+        }
 
+        return $collecion;
+    }
 }
