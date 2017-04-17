9944d5d160faf5338146a62c65bdf49c2be5a5cc
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-17 21:28:32 +0200

    basic implementation of scraper

--- src/Club/ClubScraper.php
+++ src/Club/ClubScraper.php
@@ -2,7 +2,23 @@
 
 namespace Joe\Club;
 
-class ClubScraper
+use Joe\Connection;
+use Joe\Content\Scraper\AbstractScraper;
+use Joe\Content\Scraper\PrepareableTrait;
+use simplehtmldom_1_5\simple_html_dom;
+
+class ClubScraper extends AbstractScraper
 {
-    
+    use PrepareableTrait;
+
+    public function getAddress()
+    {
+        return Connection::ADDRESS . '/klub/' . $this->id . 'strona/1';
+    }
+
+    protected function contentHtml(simple_html_dom $fullHtml = null)
+    {
+        return $fullHtml->find('#main', 0);
+    }
+
 }
