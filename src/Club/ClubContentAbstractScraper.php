69df6570a065a745b587895f184e4567fb90c908
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-19 20:22:56 +0200

    added ClubContentAbstractScraper class

---
+++ src/Club/ClubContentAbstractScraper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Joe\Club;
+
+use Joe\Http\Client;
+use Joe\Http\GetPageTrait;
+
+class ClubContentAbstractScraper
+{
+    use GetPageTrait;
+
+    public function __construct($clubId, Client $client)
+    {
+        $this->clubId = $clubId;
+        $this->client = $client;
+    }
+}
