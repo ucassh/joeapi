6412da4cc22ea553ede177ef4feae31ee5d5882f
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-24 22:23:36 +0200

    temporary entity class

---
+++ src/Archive/ArchiveEntryEntity.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Joe\Archieve;
+
+//TODO migrate this into ArtSnippet (but first HARD refactor of ArtSnippet)
+class ArchiveEntryEntity
+{
+    /** @var \DateTime */
+    private $date;
+    /** @var string */
+    private $url;
+    /** @var string */
+    private $name;
+    /** @var int */
+    private $okCount;
+    /** @var int */
+    private $watchCount;
+    /** @var int */
+    private $commentsCount;
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $date
+     * @return ArchiveEntryEntity
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return ArchiveEntryEntity
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return ArchiveEntryEntity
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOkCount()
+    {
+        return $this->okCount;
+    }
+
+    /**
+     * @param int $okCount
+     * @return ArchiveEntryEntity
+     */
+    public function setOkCount($okCount)
+    {
+        $this->okCount = $okCount;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWatchCount()
+    {
+        return $this->watchCount;
+    }
+
+    /**
+     * @param int $watchCount
+     * @return ArchiveEntryEntity
+     */
+    public function setWatchCount($watchCount)
+    {
+        $this->watchCount = $watchCount;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCommentsCount()
+    {
+        return $this->commentsCount;
+    }
+
+    /**
+     * @param int $commentsCount
+     * @return ArchiveEntryEntity
+     */
+    public function setCommentsCount($commentsCount)
+    {
+        $this->commentsCount = $commentsCount;
+        return $this;
+    }
+
+
+
+}
