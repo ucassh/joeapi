0f955bd211022b88ba48194c4e82ccd801689720
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-25 16:20:21 +0200

    ArchiveEntryFactory added

---
+++ src/Archive/ArchiveEntryFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Joe\Club\Forum;
+
+use Joe\Archieve\ArchiveEntryEntity;
+
+class ArchiveEntryFactory
+{
+    /**
+     * @param array $params
+     * @return ArchiveEntryEntity
+     */
+    public function create(array $params = [])
+    {
+        return (new ArchiveEntryEntity())
+            ->setCommentsCount($params['comments_count'] ?: 0)
+            ->setDate(new \DateTime($params['date'] ?: null))
+            ->setName($params['name'] ?: '')
+            ->setOkCount($params['ok_count'] ?: 0)
+            ->setUrl($params['url'] ?: '')
+            ->setViewsCount($params['views_count'] ?: 0);
+    }
+}
