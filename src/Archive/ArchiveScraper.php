7b0e9d9c9aa2f31fe10138b101a150fe8aa47c3d
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-26 08:06:49 +0200

    archive scraper

--- src/Archive/ArchiveScraper.php
+++ src/Archive/ArchiveScraper.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace Joe\Archieve;
+namespace Joe\Archive;
 
+use Joe\Connection;
 use Joe\Content\Scraper\AbstractScraper;
 use Joe\Http\GetPageTrait;
+use simplehtmldom_1_5\simple_html_dom_node;
 
 class ArchiveScraper extends AbstractScraper
 {
@@ -22,6 +24,20 @@ class ArchiveScraper extends AbstractScraper
     protected function scrapInterval($year, $month)
     {
         $html = $this->getPage('http://joemonster.org/archiwum.php?sa=show_month&year='.$year.'&month='.$month);
-        $entries = $html->find('#archiwum tbody tr');
+        $factory = new ArchiveEntryFactory();
+        return array_map(
+            function (simple_html_dom_node $row) use ($factory) {
+                $link = $row->find('td>a', 0);
+                return $factory->create([
+                    'comments_count' => trim($row->find('td', 2)->text()),
+                    'date' => trim($row->find('td', 0)->text()),
+                    'ok_count' => trim($row->find('td', 4)->text()),
+                    'views_count' => str_replace(' ', '', trim($row->find('td', 3)->text())),
+                    'name' => trim($link->text()),
+                    'url' => Connection::ADDRESS .substr($link->href, 0, strrpos($link->href, '/'))
+                ]);
+            },
+            $html->find('#archiwum tbody tr')
+        );
     }
 }
