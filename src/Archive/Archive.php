680906fcd12825ca1679d98ef8bdc9e75ca4d2b2
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-26 08:07:35 +0200

    added scraper to Archive class

--- src/Archive/Archive.php
+++ src/Archive/Archive.php
@@ -1,16 +1,26 @@
 <?php
 
-namespace Joe\Archieve;
+namespace Joe\Archive;
+
+use Joe\Http\Client;
 
 class Archive
 {
     /** @var  [] */
     private $data;
+    /** @var Client */
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
 
     public function getMonth($year, $month)
     {
         if (!isset($this->data[$year][$month])) {
-            // scrap month
+            $this->data[$year][$month] = (new ArchiveScraper(null, $this->client))->scrapMonth($year, $month);
         }
 
         return $this->data[$year][$month];
@@ -19,7 +29,7 @@ class Archive
     public function getYear($year)
     {
         if (!isset($this->data[$year]) && (count($this->data[$year]) < 12 || $year != date('Y'))) {
-            // scrap year
+            $this->data[$year] = (new ArchiveScraper(null, $this->client))->scrapYear($year);
         }
 
         $this->data[$year];
