7b72139a7b8095c77c5a7a46cd21ce86239d59e3
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-28 15:34:58 +0200

    added class with content types

---
+++ src/Type.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Joe;
+
+abstract class Type
+{
+    const ARTICLE = 0;
+    const MOVIE = 1;
+    const GAL_IMAGE = 2;
+    const GAME = 3;
+    const CLOSET_IMAGE = 4;
+    const CLOSET_MUSIC = 5;
+    const CLOSET_MOVIE = 6;
+    const LINK = 7;
+    const BLOG = 8;
+}
