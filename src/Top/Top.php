af2ace9d49a3017bd1558427cf5d98a6e768c59b
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-04-30 23:45:22 +0200

    Top module main constants

---
+++ src/Top/Top.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Joe\Top;
+
+class Top
+{
+    const TODAY = 'day';
+    const THIS_WEEK = 'week';
+    const THIS_MONTH = 'month';
+    const YEAR = 'year';
+    const ALWAYS = 'all';
+}
