3b7187d041e50607db33950a4170ea6c8779f5cd
Łukasz Kaczmarek <lukasz.s.kaczmarek@gmail.com>
2017-05-03 22:05:45 +0200

    TrolledScraper with basic mechanism; only extractor is missing

--- src/Trolled/TrolledScraper.php
+++ src/Trolled/TrolledScraper.php
@@ -4,6 +4,7 @@ namespace Joe\Trolled;
 
 use Joe\Content\Scraper\AbstractScraper;
 use Joe\Http\Scraper\ScrapPagesInterface;
+use simplehtmldom_1_5\simple_html_dom_node;
 
 class TrolledScraper extends AbstractScraper implements ScrapPagesInterface
 {
@@ -11,7 +12,13 @@ class TrolledScraper extends AbstractScraper implements ScrapPagesInterface
     public function scrapPage($pageId)
     {
         $html = $this->getPage($this::ADDRESS . '/gadki/' . $pageId);
+        $talks = $html->find('div.singleTalk');
+        return array_map([$this, 'extractEntry'], $talks ?: []);
+    }
 
+    protected function extractEntry(simple_html_dom_node $node)
+    {
+        return [];
     }
 
     public function maxItems()
